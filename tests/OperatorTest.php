680e77203e8cf9fec1f4c068b2c9b5c251909562
tsmsogn <tsmsogn@gmail.com>
2016-05-17 12:04:54 +0900

    Use phpunit

---
+++ tests/OperatorTest.php
@@ -0,0 +1,32 @@
+<?php
+class Foo {
+    public $bar;
+
+    public function __construct($bar = null) {
+        $this->bar = $bar;
+    }
+}
+
+class OperatorTest extends PHPUnit_Framework_TestCase {
+
+    public function testComparision() {
+        $foo1 = new Foo(1);
+        $foo2 = new Foo();
+        $foo3 = new Foo();
+
+        $this->assertFalse($foo1 == $foo2);
+        $this->assertFalse($foo1 == $foo2);
+        $this->assertFalse($foo1 === $foo2);
+        $this->assertTrue($foo2 == $foo3);
+    }
+
+    public function testReference() {
+        $a =& $c;
+        $b = $c;
+        $c = 1;
+
+        $this->assertEquals(1, $a);
+        $this->assertEquals(null, $b);
+        $this->assertEquals(1, $c);
+    }
+}
